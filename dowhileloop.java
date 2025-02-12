b008ade065584962b463e9ab2281e7580526d54f
TIRTH(TD) <145533258+TD2212@users.noreply.github.com>
2025-02-12 11:47:26 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,12 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        //Do while loop
+        //It run 1's time either ronge and write
+        int i=10;
+        do{
+            System.out.println("Number is"+i);
+            i++;
+        }
+        while(i<1);
+    }
+}
