79522abedfd06f4d33221ad2a2b1eb54af08c073
TIRTH(TD) <145533258+TD2212@users.noreply.github.com>
2025-02-12 11:42:44 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,14 @@
+public class whileloop {
+    public static void main(String[] args) {
+        //print 1 to 100
+        int i=1;
+        
+        while(i<=100)
+        {
+            System.out.println("Number"+i);
+        
+        i++;
+        }
+    }
+}    
+
