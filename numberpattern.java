935d21859e4bdd9c5587817d8a7e67b48b4e9738
TIRTH(TD) <145533258+TD2212@users.noreply.github.com>
2025-02-19 12:45:22 +0530

    Add files via upload

---
+++ numberpattern.java
@@ -0,0 +1,13 @@
+public class numberpattern {
+    public static void main(String[] args) {
+        int num=5;
+        for(int i=0;i<=num;i++)
+        {
+            for(int j=i;j>0;j--)
+            {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
+    }
+}
