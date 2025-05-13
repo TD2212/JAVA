549a87821cac1014798e5f12a8db8dbb1f07971e
TIRTH(TD) <145533258+TD2212@users.noreply.github.com>
2025-05-13 12:54:40 +0530

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class primenumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number = ");
+        int num = sc.nextInt();
+        sc.close();
+
+        boolean isPrime = true;
+
+        // Check for numbers less than or equal to 1
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            // Check for divisibility from 2 to sqrt(num)
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        // Output the result
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
