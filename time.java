d29e94246317e1a0ca4289f182e0956fb5654a79
TIRTH(TD) <145533258+TD2212@users.noreply.github.com>
2025-06-25 12:05:05 +0530

    Add files via upload

---
+++ time.java
@@ -0,0 +1,5 @@
+public class time {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);//1000 for secend//3600 for hour//24 for days//365 for year
+    }
+}
