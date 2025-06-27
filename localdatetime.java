a7bb85fa6cdfdbf7212197bacffafeab3a0c57f3
TIRTH(TD) <145533258+TD2212@users.noreply.github.com>
2025-06-27 12:58:23 +0530

    Update localdatetime.java

--- localdatetime.java
+++ localdatetime.java
@@ -1,8 +1,14 @@
 
+
+import java.text.DateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class localdatetime {
     public static void main(String[] args) {
         LocalDateTime dt=  LocalDateTime.now();
         System.out.println(dt);
+        DateTimeFormatter df= DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String mydate=df.format(dt);
+        System.out.println(mydate);
     }
 }
